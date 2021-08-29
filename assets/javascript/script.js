11f511d1a7bad52caf507115253317ba24d07d47
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-08-29 09:10:11 -0600

    setting up javascript file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,3 +2,25 @@
 
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
+
+//notes here
+const app = express();
+const PORT = process.env.PORT || 4001
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Starts the server
+app.listen(PORT, function () {
+    console.log(`Note Taker Server listening on PORT: ${PORT} open the app here: http://localhost:${PORT}`);
+});
