cd5b5b73d929a1ee0dc0f5de5a4ee7dd61e9c79e
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-08-29 20:23:52 -0600

    updating js

--- js/server.js
+++ js/server.js
@@ -19,4 +19,65 @@ app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+//Starts the server
+app.listen(PORT, function () {
+    console.log(`Note Taker Server listening on PORT: ${PORT} open the app here: http://localhost:${PORT}`);
+});
+
+
+//-----=======++++++-----=======++++++-----=======++++++-----=======++++++
+//user guide for this project
+//-----=======++++++-----=======++++++-----=======++++++-----=======++++++
+
+//steps & commands 
+//npm install
+//npm install node
+// npm install express --save
+//npm install - g nodemon
+//nodemon [your node app]
+
+//npm start
+
+// node app.js
+//run npm init
+
+// npm init
+
+
+//npm i nodemon
+
+
+
+//nodemon. / server.js localhost 8080
+
+
+
+//-----=======++++++-----=======++++++-----=======++++++-----=======++++++
+
+//resources
+//https: //expressjs.com/
+// to install express https://expressjs.com/en/starter/installing.html
+//node needs to be installed https://nodejs.org/en/
+//https: //www.npmjs.com/package/nodemon
+//https: //www.npmjs.com/package/express
+
+//-----=======++++++-----=======++++++-----=======++++++-----=======++++++
+
+//info
+//express is a framework for node js
+
+
+// {
+//   "name": "nodemon",
+//   "homepage": "http://nodemon.io",
+//   "...": "... other standard package.json values",
+//   "nodemonConfig": {
+//     "ignore": ["test/*", "docs/*"],
+//     "delay": 2500
+//   }
+// }
+
+
+//nodemon server.js
+//nodemon server.js 3006
+//https://www.digitalocean.com/community/tutorials/workflow-nodemonno
