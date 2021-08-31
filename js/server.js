f2d5a023f2cf1c80b1ad48f8c6f0a973465cbff9
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-08-31 10:02:11 -0600

    ok

--- js/server.js
+++ js/server.js
@@ -1,5 +1,10 @@
 // Dependencies
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const nodemon = require("nodemon");
+
 const apiRoutes = require("./routes/apiRoutes.js");
 const htmlRoutes = require("./routes/htmlRoutes.js");
 
@@ -16,12 +21,41 @@ app.use(express.urlencoded({
 }));
 
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.json());
+app.use("/assets", express.static("./assets"));
+
+app.use("/api", apiR;
+app.use("/", htmlR);
+
+require("./routing/html-routes")(app);
+require("./routing/api-routes")(app);
+
+// ROUTING
+
+module.exports = (app) => {
+    // => HTML GET Requests
+//app.get('/', (req, res) => {
+//res.send('Welcome to the Star Wars Page!');
+//});
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+//app.get('/yoda', (req, res) => {
+//res.json(yoda);
+//});
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+    // If no matching route is found default to home
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+};
 
 //Starts the server
 app.listen(PORT, function () {
-    console.log(`Note Taker Server listening on PORT: ${PORT} open the app here: http://localhost:${PORT}`);
+    console.log(`Note Taker listening on PORT: ${PORT} open the app here: http://localhost:${PORT}`);
 });
 
 
