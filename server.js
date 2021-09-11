8a8cbac47dae122fc30d57eef1df9444e40edc94
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-09-11 17:11:55 -0600

    notes added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+
+//urlencoded() function = a built-in middleware function in Express
+//https://www.google.com/search?q=app.use(express.urlencoded(%7B&rlz=1C5CHFA_enUS956US956&oq=app.use(express.urlencoded(%7B&aqs=chrome..69i57j0i512l3j0i30l3.1374j0j15&sourceid=chrome&ie=UTF-8
 app.use(express.urlencoded({
     extended: true
 }));
