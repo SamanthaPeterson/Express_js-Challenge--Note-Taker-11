83423afe872abb27acc338f99a0c3c19d8e79a30
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-09-11 23:06:13 -0600

    ok

--- server.js
+++ server.js
@@ -13,12 +13,17 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+//adds a middleware - To serve static files such as images, CSS files, and JavaScript file
+// https: //www.google.com/search?q=app.use(express.static&rlz=1C5CHFA_enUS956US956&oq=app.use(express.static&aqs=chrome..69i57j0i512l9.1090j0j15&sourceid=chrome&ie=UTF-8
 app.use(express.static('public'));
 app.use(express.json());
+//
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-
+//function is used to bind and listen the connections on the specified host and port
+//https: //www.google.com/search?q=app.listen(PORT,+()+%3D%3E+%7B&rlz=1C5CHFA_enUS956US956&ei=UTg9Ydy9Bo_L0PEPnIqtgA8&oq=app.listen(PORT,+()+%3D%3E+%7B&gs_lcp=Cgdnd3Mtd2l6EAMyCAgAEIAEELADMggIABCABBCwAzIICAAQgAQQsAMyCAgAEIAEELADMggIABCABBCwAzIICAAQgAQQsAMyCAgAEIAEELADMggIABCABBCwAzIHCAAQsAMQHjIICAAQgAQQsAMyCAgAEOQCELADMggIABDkAhCwAzIICAAQ5AIQsANKBQg8EgExSgQIQRgBULrxfli68X5g9fd-aAFwAHgAgAEAiAEAkgEAmAEAoAECoAEByAENwAEB&sclient=gws-wiz&ved=0ahUKEwicqs6khvjyAhWPJTQIHRxFC_AQ4dUDCA4&uact=5&pccc=1
 app.listen(PORT, () => {
+    //debugging
     console.log(`API server now on port ${PORT}!`);
 });
